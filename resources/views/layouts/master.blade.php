a8a4423f4977000bcef9120ea5fa0602609566c8
vincent hoogstra <vincent_Hoogstra@live.nl>
2018-11-12 19:35:28 +0100

    styling done on menu phone

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,35 +2,35 @@
 
 <div class="container content-wrapper" id="main">
 
-<div class="modal fade" id="LogoutModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-     aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a href="{{ route('logout') }}" class="btn btn-primary"
-                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    Logout
-                </a>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
+    <div class="modal fade" id="LogoutModel" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a href="{{ route('logout') }}" class="btn btn-primary"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </div>
             </div>
         </div>
     </div>
+    <div class="content-wrapper">
+        <div class="container-fluid">
+            <!-- Icon Cards-->
+            @yield('content')
+        </div>
+    </div>
 </div>
-<div class="content-wrapper">
-    <div class="container-fluid">
-        <!-- Icon Cards-->
-    @yield('content')
-
-</div>
-
 @include('partials.footer')
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,25 +1,45 @@
-
-        <!-- /.container-fluid-->
-        <!-- /.content-wrapper-->
-        <footer class="sticky-footer">
-                <div class="text-center">
-                    <small>Copyright © dezlv.nl 2018 {{ config('app.name', 'Laravel') }} V{{ config('app.version', 'Laravel') }}</small>
-                </div>
-        </footer>
-        <!-- Logout Modal-->
-        <!-- Bootstrap core JavaScript-->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js"></script>
-        <!-- Core plugin JavaScript-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
-        <!-- Page level plugin JavaScript-->
-        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
-        <!-- Custom scripts for all pages-->
-        {{--<script src="/js/sb-admin.min.js"></script>--}}
-        <!-- Custom scripts for this page-->
-        {{--<script src="/js/sb-admin-datatables.min.js"></script>--}}
+<!-- /.container-fluid-->
+<!-- /.content-wrapper-->
+<footer class="sticky-footer">
+    <div class="text-center">
+        <small>Copyright © dezlv.nl 2018 {{ config('app.name', 'Laravel') }}
+            V{{ config('app.version', 'Laravel') }}</small>
     </div>
-</div>
+</footer>
+<!-- Logout Modal-->
+<!-- Bootstrap core JavaScript-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js"></script>
+<!-- Core plugin JavaScript-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+<!-- Page level plugin JavaScript-->
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+<!-- Custom scripts for all pages-->
+{{--<script src="/js/sb-admin.min.js"></script>--}}
+<!-- Custom scripts for this page-->
+{{--<script src="/js/sb-admin-datatables.min.js"></script>--}}
+<script>
+    var menuState = true;
+    $(document).ready(function () {
+        console.log(menuState);
+    });
+    // $('#menuColapase').click();
+    $('#menuColapase').click(function () {
+
+        console.log(menuState);
+        if (menuState) {
+            $('.nav-side-menu').animate({height: '100%'});
+
+            menuState = false;
+        } else {
+            $('.nav-side-menu').animate({height: '51px'});
+
+            menuState = true;
+        }
+    });
+    // nav-side-menu
+
+</script>
 </body>
 
 </html>
