055dca194075c4c3df1001b5097c689b3db8e53c
Unknown <vincent_Hoogstra@live.nl>
2018-11-23 21:09:27 +0100

    layout fix

--- resources/views/log/index.blade.php
+++ resources/views/log/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master') @section('content')
 
     <div class="h4">
-        <i class="fas fa-hdd fa-lg"></i> {{$categoryActive}}
+        <i class="fas fa-hdd fa-lg"></i>
     </div>
     <hr>
     <div class="top-area">
@@ -11,7 +11,7 @@
             @else
                 <li>
                     @endif
-                <a href="{{route('log.index')}}">all
+                <a href="{{route('log.index')}}">All
                     <span class="badge badge-pill">{{$AllLogCount}}</span>
                 </a>
             </li>
@@ -36,50 +36,34 @@
            role="grid" aria-describedby="dataTable_info" style="width: 100%;">
         <thead>
         <tr role="row">
-            <th rowspan="1" colspan="1">logged user</th>
-            <th rowspan="1" colspan="1">category</th>
-            <th rowspan="1" colspan="1">info</th>
-            <th rowspan="1" colspan="1">var</th>
+            <th rowspan="1" colspan="1">User</th>
+            <th rowspan="1" colspan="1">Date</th>
+            <th rowspan="1" colspan="1">Category</th>
+            <th rowspan="1" colspan="1">Text</th>
         </tr>
         </thead>
         <tfoot>
         <tr>
-            <th rowspan="1" colspan="1">logged user</th>
-            <th rowspan="1" colspan="1">category</th>
-            <th rowspan="1" colspan="1">info</th>
-            <th rowspan="1" colspan="1">var</th>
+            <th rowspan="1" colspan="1">User</th>
+            <th rowspan="1" colspan="1">Date</th>
+            <th rowspan="1" colspan="1">Category</th>
+            <th rowspan="1" colspan="1">Text</th>
         </tr>
         </tfoot>
         <tbody>
         @foreach($log as $log)
             <tr>
                 <td>{{$log->user->name}}</td>
+                <td>{{$log->created_at}}</td>
                 <td>{{$log->logCategory->name}}</td>
                 <td>{{$log->info}}</td>
-                <td>{{$log->var}}</td>
             </tr>
         @endforeach
         </tbody>
     </table>
-
-
     <script>
-
-
         $(document).ready(function () {
             var table = $('#datatable').DataTable();
         });
-
-        //and delete
-        {{--<form action="{{route("party.destroy", ['id' =>$party->id])}}"--}}
-        {{--method="POST" class="btn-group">--}}
-        {{--<button type="submit" class="btn btn-danger">--}}
-        {{--<i class="fa fa-trash-o"></i> Remove--}}
-        {{--</button>--}}
-
-        {{--@CSRF--}}
-        {{--@method('DELETE')--}}
-        {{--</form>--}}
-
     </script>
 @endsection
